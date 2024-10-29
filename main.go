633bf7ac8b10c07ca687ca1e38194c725db5c3b6
pman <pman-$-432894328923@gmail.com>
2024-10-29 22:52:42 +0100

    chore: update file(s): main.go

--- main.go
+++ main.go
@@ -6,6 +6,30 @@ import (
 	"time"
 )
 
+func doSomethingDeadline(ctx context.Context) {
+	deadline := time.Now().Add(1500 * time.Millisecond)
+	ctx, cancelCtx := context.WithDeadline(ctx, deadline)
+	defer cancelCtx()
+
+	printCh := make(chan int)
+	go doAnother(ctx, printCh)
+
+	for num := 1; num <= 3; num++ {
+		select {
+		case printCh <- num:
+			time.Sleep(1 * time.Second)
+		case <-ctx.Done():
+			break
+		}
+	}
+
+	cancelCtx()
+
+	time.Sleep(100 * time.Millisecond)
+
+	fmt.Printf("doSomething: finished\n")
+}
+
 func doSomething(ctx context.Context) {
 	ctx, cancelCtx := context.WithCancel(ctx)
 
