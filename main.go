5cc9167e39fc3b2efb395c8e4ba4272360c29621
pman <pman-$-432894328923@gmail.com>
2024-10-29 22:10:57 +0100

    chore: update file(s): main.go

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
-  "context"
+	"context"
+	"fmt"
+	"time"
 )
 
 func main() {
@@ -9,5 +11,25 @@ func main() {
 }
 
 func exampleTimeout() {
-  ctx := context.Background()
+	// Initialize empty context struct.
+	ctx := context.Background()
+
+	ctxWithTimeout, cancel := context.WithTimeout(ctx, 2*time.Second)
+	defer cancel()
+
+	done := make(chan struct{})
+
+	go func() {
+		time.Sleep(3 * time.Second)
+		close(done)
+	}()
+
+  select {
+  case <- done:
+    fmt.Println("Called the API")
+
+  case <- ctxWithTimeout.Done():
+    fmt.Println("Timeout expired...", ctxWithTimeout.Err())
+    // Do something to handle.
+  }
 }
